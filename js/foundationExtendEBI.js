3b94cf57b0b1609a93d12c108fcc4cbe2e2aa754
Ken Hawkins <khawkins98@gmail.com>
2016-05-20 14:51:17 +0100

    Clearinput: visual tweaks

--- js/foundationExtendEBI.js
+++ js/foundationExtendEBI.js
@@ -16,7 +16,7 @@
   $(document).on('input', '.clearable', function(){
     $(this)[tog(this.value)]('x');
   }).on('mousemove', '.x', function( e ){
-    $(this)[tog(this.offsetWidth-18 < e.clientX-this.getBoundingClientRect().left)]('onX');
+    $(this)[tog(this.offsetWidth-25 < e.clientX-this.getBoundingClientRect().left)]('onX');
   }).on('touchstart click', '.onX', function( ev ){
     ev.preventDefault();
     $(this).removeClass('x onX').val('').change().keyup();
