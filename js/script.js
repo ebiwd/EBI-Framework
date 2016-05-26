ffca1760a6392d0dbfdab22dc07042a71dcaf6ea
khawkins98 <khawkins98@gmail.com>
2016-05-26 12:43:19 +0100

    Oops

--- js/script.js
+++ js/script.js
@@ -442,7 +442,7 @@ $.fn.foundationExtendEBI = function() {
     $('a[href*=#]:not([href=#])').click(function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
         var target = $(this.hash);
-        window.location.hash = target;
+        window.location.hash = this.hash;
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
         if (target.length) {
           $('html,body').animate({
