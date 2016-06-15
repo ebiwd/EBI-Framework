aaf7a07f83a0a962d71a43c371b36e48b606b3ca
khawkins <khawkins98@gmail.com>
2016-06-15 03:04:12 +0100

    Local menu responsify: add size change direction

--- js/foundationExtendEBI.js
+++ js/foundationExtendEBI.js
@@ -14,8 +14,11 @@
   var localMenuWidthAvail = $('#local-masthead .masthead > nav').width();
   // var localMenuWidthTotal = $(localMenuClass).width();
 
-  function localNavSpilloverMenu() {
+  function localNavSpilloverMenu(changeDirection) {
     var localMenuWidthUsed = 0;
+    var localMenuWidthNotUsed = localMenuWidthAvail;
+    var localMenuRightSideWidth = $('#local-masthead .masthead > nav ul.float-right.menu').width(); // width of any right-side nav, this could change on browser resize
+    localMenuRightSideWidth = localMenuRightSideWidth + 5; // padding
 
     // Calculate how much space we need, if any
     $(localMenuClass+' > li').each( function() {
@@ -24,29 +27,39 @@
 
     // account for any float-right menu
     // to do: account for any float-right menu
-    localMenuWidthAvail = localMenuWidthAvail - 0;
+    localMenuWidthNotUsed = localMenuWidthNotUsed - localMenuRightSideWidth;
 
     // do we need to make space?
-    if (localMenuWidthUsed > localMenuWidthAvail) {
+    if (changeDirection != 'increase') {
+      if (localMenuWidthUsed > localMenuWidthNotUsed) {
+        // create dropdown if needed
+        if ($(localMenuClass + ' li.extra-items-menu').length == 0) { 
+          $(localMenuClass).append('<li class="extra-items-menu"><a href="#">Also in this section</a><ul class="menu"></ul></li>');
+          // to do: should we be invoking foundation to create/destroy the dropdown functionality when we add/remove the menu?
+          localMenuWidthUsed = localMenuWidthUsed + $(localMenuClass + ' li.extra-items-menu').width();
+        }
 
-      // create dropdown if needed
-      if ($(localMenuClass + ' li.extra-items-menu').length == 0) { 
-        $(localMenuClass).append('<li class="extra-items-menu"><a href="#">Also in this section</a><ul class="menu"></ul></li>');
-        // to do: should we be invoking foundation to create/destroy the dropdown functionality when we add/remove the menu?
-        localMenuWidthUsed = localMenuWidthUsed + $(localMenuClass + ' li.extra-items-menu').width();
-      }
+        // loop through each menu item in reverse, and slice off the first as it's the dropdown
+        $($(localMenuClass+' > li').get().reverse().slice(1)).each( function() {
+          if (localMenuWidthUsed > localMenuWidthNotUsed) { // do we need to hide more items?
+            localMenuWidthUsed = localMenuWidthUsed - $(this).width();
+            $(this).detach().prependTo(localMenuClass + ' li.extra-items-menu ul.menu');
+          } // we could break when <= but this should be pretty fast
+        });
+      } 
+    }
 
-      // loop through each menu item in reverse, and slice off the first as it's the dropdown
-      $($(localMenuClass+' > li').get().reverse().slice(1)).each( function() {
-        if (localMenuWidthUsed > localMenuWidthAvail) { // do we need to hide more items?
-          localMenuWidthUsed = localMenuWidthUsed - $(this).width();
-          $(this).detach().prependTo(localMenuClass + ' li.extra-items-menu ul.menu');
-        } // we could break when <= but this should be pretty fast
-      });
-    } else {
-      // if the menu is shorter than full width, we can perhaps restore some menu items from the dropdown
-      if ($(localMenuClass + ' li.extra-items-menu').length > 0) { // does the dropdown exist?
-        // to do: restore menu items with any free space 
+    if (changeDirection == 'increase') {
+
+      // does the dropdown exist?
+      if ($(localMenuClass + ' li.extra-items-menu').length > 0) { 
+
+        // if the menu is shorter than full width, we can perhaps restore some menu items from the dropdown
+
+        // if we delete the menu
+        localMenuWidthNotUsed = localMenuWidthNotUsed + $(localMenuClass + ' li.extra-items-menu').width();
+
+        // console.log('if delete drop down, we have free space of',localMenuWidthNotUsed);
 
         if ($(localMenuClass + ' li.extra-items-menu li').length == 0) { 
           // if the dropdown is empty, remove it
@@ -54,15 +67,17 @@
         }
       }
     }  
+
   }
 
-  localNavSpilloverMenu();
+  localNavSpilloverMenu('init');
   // re-calc menus on browser change, if it affect width of localMenuWidthAvail
   $(window).resize( function() {
-    if ($('#local-masthead .masthead > nav').width() != localMenuWidthAvail) {
-      localMenuWidthAvail = $('#local-masthead .masthead > nav').width();
-      localNavSpilloverMenu();
-    }
+    var widthChangeAmount = $('#local-masthead .masthead > nav').width() - localMenuWidthAvail;
+    if (widthChangeAmount != 0) localMenuWidthAvail = $('#local-masthead .masthead > nav').width();
+    // we look for changes of more than 1 to reduce jitter
+    if (widthChangeAmount > 1)  localNavSpilloverMenu('increase');
+    if (widthChangeAmount < 1)  localNavSpilloverMenu('decrease');
   });
 
   // Clearable text inputs
