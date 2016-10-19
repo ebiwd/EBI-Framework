39f803ba6e8b60ce568d4f090ee08e58ab07c757
Ken Hawkins <khawkins98@gmail.com>
2016-10-19 20:52:14 +0100

    More clarity in redirect
    To address issue as observed in: https://github.com/ebiwd/EBI-Framework/issues/43

--- sample-site/migrations/testMigration.js
+++ sample-site/migrations/testMigration.js
@@ -1,6 +1,9 @@
 /*
   This is the old script.
+  Read about the script at: https://github.com/ebiwd/EBI-Pattern-library/blob/gh-pages/sample-site/migrations/testMigration.js
  */
 console.log('This is the old script, please be sure to use the new one.');
-console.log('Details at: https://github.com/ebiwd/EBI-Pattern-library/blob/gh-pages/sample-site/migrations/testMigration.js');
-
+console.log('You can invoke the new script with:');
+console.log("jQuery('head').append('<script defer=\"defer\" src=\"//ebiwd.github.io/EBI-Pattern-library/sample-site/migrations/testMigration.js\"></script>\/n'\)\;");
+console.log('------');
+console.log('Questions? Read about how to use the script at: https://github.com/ebiwd/EBI-Pattern-library/blob/gh-pages/sample-site/migrations/testMigration.js');
