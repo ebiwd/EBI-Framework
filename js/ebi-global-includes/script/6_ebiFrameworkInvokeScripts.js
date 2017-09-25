73b7cc14d4b3f710b97128dbac59f010aa555c07
khawkins98 <khawkins98@gmail.com>
2017-09-25 10:21:04 +0100

    Update transitionary black bar vision

--- js/ebi-global-includes/script/6_ebiFrameworkInvokeScripts.js
+++ js/ebi-global-includes/script/6_ebiFrameworkInvokeScripts.js
@@ -6,6 +6,7 @@
  */
 function ebiFrameworkInvokeScripts() {
   ebiFrameworkPopulateBlackBar();
+  ebiFrameworkActivateBlackBar();
   ebiFrameworkExternalLinks();
   ebiFrameworkManageGlobalSearch();
   ebiFrameworkSearchNullError();
