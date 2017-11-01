4f0993fcf2e3aa81bd12ec71a63942a39ad6ffc2
khawkins98 <khawkins98@gmail.com>
2017-11-01 10:03:51 +0000

    Typo

--- js/elixirBanner.js
+++ js/elixirBanner.js
@@ -24,7 +24,7 @@ function elixirBanner() {
         defaultDescription = divElixirBanner.dataset.description;
       }
       if (typeof divElixirBanner.dataset.moreInformationLink !== "undefined") {
-        defualtLink = divElixirBanner.dataset.moreInformationLink;
+        defaultLink = divElixirBanner.dataset.moreInformationLink;
       }
       if (divElixirBanner.dataset.useBasicStyles === 'true') {
         basicStylingForNonfoundationSites = `
