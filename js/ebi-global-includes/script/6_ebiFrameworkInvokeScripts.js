fd6ed9c4c72d89de7f523172c50ae8fb4c484af4
Ken Hawkins <khawkins98@gmail.com>
2018-05-21 13:39:46 +0100

    Add data protection banner to v1.2 #137

--- js/ebi-global-includes/script/6_ebiFrameworkInvokeScripts.js
+++ js/ebi-global-includes/script/6_ebiFrameworkInvokeScripts.js
@@ -1,3 +1,16 @@
+// Use the v1.3 data protection banner on older sites
+function legacyDataProtectionBanner() {
+  var localFrameworkVersion = '1.2'; // 1.1 or 1.2 or compliance or other
+      // if you select compliance or other we will add some helpful
+      // CSS styling, but you may need to add some CSS yourself
+  var newDataProtectionNotificationBanner = document.createElement('script');
+  newDataProtectionNotificationBanner.src = 'https://dev.ebi.emblstatic.net/web_guidelines/EBI-Framework/v1.3/js/ebi-global-includes/script/5_ebiFrameworkNotificationBanner.js?legacyRequest='+localFrameworkVersion;
+  document.head.appendChild(newDataProtectionNotificationBanner);
+  newDataProtectionNotificationBanner.onload = function() {
+    ebiFrameworkRunDataProtectionBanner(); // invoke the banner
+  };
+}
+
 function ebiFrameworkInvokeScripts() {
   ebiFrameworkExternalLinks();
   ebiFrameworkManageGlobalSearch();
@@ -8,7 +21,8 @@ function ebiFrameworkInvokeScripts() {
   ebiFrameworkUpdateFoot();
   ebiFrameworkUpdateFooterMeta();
   ebiFrameworkIncludeScripts();
-  ebiFrameworkCookieBanner();
+  // ebiFrameworkCookieBanner();
+  legacyDataProtectionBanner();
 }
 
 /* All scripts are automatically loaded, unless the page asked us not to.
