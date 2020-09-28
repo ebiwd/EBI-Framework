31dc4fd2cf31da8cba9ec1f41adb886cc5af3ae6
Ken Hawkins <khawkins98@gmail.com>
2020-09-28 13:20:44 +0200

    Cookie banner look

--- js/ebi-global-includes/script/5_ebiFrameworkNotificationBanner.js
+++ js/ebi-global-includes/script/5_ebiFrameworkNotificationBanner.js
@@ -14,7 +14,7 @@ function ebiFrameworkCreateDataProtectionBanner() {
 
   banner.id = "data-protection-banner";
   banner.className = "data-protection-banner";
-  banner.style.cssText = "position: fixed; background: #111; width: 100%; padding: .75rem 1%; left: 0; bottom: 0; color: #eee; z-index: 10;";
+  banner.style.cssText = "position: fixed; background: #707372; width: 100%; padding: .75rem 1%; left: 0; bottom: 0; border-top: 1px solid #373a36; color: #eee; z-index: 10;";
   wrapper.className = "row";
   wrapper.innerHTML = "" +
     "<div class='columns medium-8 large-9 white-color'>" +
